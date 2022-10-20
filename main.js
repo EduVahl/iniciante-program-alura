4fa3a084fb7d3ae18b1abdcb3a6d161f3cac9782
Luís Eduardo Vahl Dias <116232111+EduVahl@users.noreply.github.com>
2022-10-20 15:54:38 -0300

    Adicionando arquivos Aluramidi via upload.

---
+++ main.js
@@ -0,0 +1,16 @@
+const listaTeclas = document.querySelectorAll('.tecla');
+const listaSons = document.querySelectorAll('.som');
+
+
+
+for(let i = 0; i < listaTeclas.length; i++){
+    listaTeclas[i].onclick = function (){listaSons[i].play();
+    }
+ 
+    listaTeclas[i].onkeydown = function (evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){ listaTeclas[i].classList.add('ativa')};
+    }
+
+    listaTeclas[i].onkeyup = function (){listaTeclas[i].classList.remove('ativa');
+    }   
+}
